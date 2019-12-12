1453b543de9ce88bf9d65363ce47b98b5ba809ca
adam <adamalfal8@mail.unpas.ac.id>
2019-12-12 18:15:14 +0700

    perbaikan class peminjaman

--- src/id/ac/psi/model/pojo/Peminjamaan.java
+++ src/id/ac/psi/model/pojo/Peminjamaan.java
@@ -5,10 +5,73 @@
  */
 package id.ac.psi.model.pojo;
 
+import java.sql.Date;
+
 /**
  *
  * @author RioErika
  */
 public class Peminjamaan {
+    private int id;
+    private Anggota anggota;
+    private Buku buku;
+    private Date tglPinjam;
+    private Date tglKembali;
+    private boolean status;
+
+    
+    
+    public Peminjamaan(){
+        
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Anggota getAnggota() {
+        return anggota;
+    }
+
+    public void setAnggota(Anggota anggota) {
+        this.anggota = anggota;
+    }
+
+    public Buku getBuku() {
+        return buku;
+    }
+
+    public void setBuku(Buku buku) {
+        this.buku = buku;
+    }
+
+    public Date getTglPinjam() {
+        return tglPinjam;
+    }
+
+    public void setTglPinjam(Date tglPinjam) {
+        this.tglPinjam = tglPinjam;
+    }
+
+    public Date getTglKembali() {
+        return tglKembali;
+    }
+
+    public void setTglKembali(Date tglKembali) {
+        this.tglKembali = tglKembali;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+    
     
 }
